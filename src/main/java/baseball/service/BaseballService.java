5c0528fd0a801696ae60c8b624b78e673b6d752d
nuyh <hjaehyun25@gmail.com>
2022-12-14 22:13:41 +0900

    feat: 유저의 값 정답 판별하기 기능 추가

---
+++ src/main/java/baseball/service/BaseballService.java
@@ -0,0 +1,25 @@
+package baseball.service;
+
+import baseball.domain.Computer;
+import baseball.domain.Result;
+import baseball.domain.User;
+
+public class BaseballService {
+    private final Computer computer;
+    private final User user;
+    private final Result result;
+
+    public BaseballService(Computer computer, User user, Result result) {
+        this.computer = computer;
+        this.user = user;
+        this.result = result;
+    }
+
+    public void input(String input) {
+        user.input(input);
+    }
+
+    public void match() {
+        result.match(computer.getAnswer(), user.getInput());
+    }
+}
