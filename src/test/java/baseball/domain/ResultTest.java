7acaffec9d1f45909156c91e93492411e220bc65
nuyh <hjaehyun25@gmail.com>
2022-12-14 22:13:53 +0900

    test: 정답 판별 테스트

---
+++ src/test/java/baseball/domain/ResultTest.java
@@ -0,0 +1,46 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResultTest {
+
+    @Test
+    void 모두_스트라이크() throws Exception {
+        //given
+        Computer computer = new Computer();
+        User user = new User();
+        Result result = new Result();
+
+        //when
+        String answer = computer.getAnswer().stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        user.input(answer);
+        result.match(computer.getAnswer(), user.getInput());
+
+        //then
+        assertThat(result.getStrikes()).isEqualTo(3);
+        assertThat(result.getBalls()).isEqualTo(0);
+    }
+
+    @Test
+    void 볼1_스트라이크1() throws Exception {
+        //given
+        Result result = new Result();
+        List<Integer> answer = List.of(1, 2, 3);
+        List<Integer> user = List.of(1, 5, 2);
+
+        //when
+        result.match(answer, user);
+
+        //then
+        assertThat(result.getStrikes()).isEqualTo(1);
+        assertThat(result.getBalls()).isEqualTo(1);
+    }
+}
