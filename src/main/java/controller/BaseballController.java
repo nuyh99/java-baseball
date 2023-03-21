02f40b094c0a7d8b8847b51eea8169262bc10766
onegqueen <rkdwldnjs30@naver.com>
2023-03-21 15:59:25 +0900

    Feat: 사용자에게 숫자 입력받는 기능 구현

--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -1,2 +1,30 @@
-package controller;public class BaseballController {
+package controller;
+
+import domain.Computer;
+import domain.User;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import view.InputView;
+
+public class BaseballController {
+    public void run() {
+        User user;
+        Computer computer = new Computer();
+
+        user = provideUserNumber(InputView.readUserNumber());
+    }
+
+    private User provideUserNumber(String input) {
+        User user = new User();
+        Stream<String> strStream = Arrays.stream(input.split(""));
+        List<Integer> userNumber = strStream.map(s->Integer.parseInt(s))
+            .collect(Collectors.toCollection(ArrayList::new));
+
+        user.setNumber(userNumber);
+        return user;
+    }
+
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,20 @@
-package view;public class InputView {
+package view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String readUserNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return readLine();
+    }
 }
