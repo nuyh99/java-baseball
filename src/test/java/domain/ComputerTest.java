6045aed812190694c5ff3bbd67e10cf5499c3854
onegqueen <rkdwldnjs30@naver.com>
2023-03-21 16:51:32 +0900

    Test: 컴퓨터가 생성한 넘버가 유효한지 검증하는 테스트

--- src/test/java/domain/ComputerTest.java
+++ src/test/java/domain/ComputerTest.java
@@ -1,2 +1,35 @@
-package domain;public class ComputerTest {
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Computer는")
+public class ComputerTest {
+
+    public static final int EXPECTED_NUMBER_SIZE = 3;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+
+    @Test
+    @DisplayName("세자리 수이며 각 자리수가 1 - 9 사이인 수를 생성한다")
+    void ComputerTest1() {
+        //given
+        Computer computer = new Computer();
+
+        //when
+        computer.createNumber();
+
+        //then
+        assertThat(computer.getnumber().size()).isEqualTo(EXPECTED_NUMBER_SIZE);
+        assertThat(computer.getnumber().stream()
+            .filter(item -> item>= MIN_VALUE || item<=MAX_VALUE)
+            .count()).isEqualTo(EXPECTED_NUMBER_SIZE);
+    }
+
+
 }
