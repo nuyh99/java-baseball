21d1668678101dfd396bccbe0e6c145662fc805b
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-02 09:19:28 +0900

    과제

---
+++ src/baseballgame/GameMessage.java
@@ -0,0 +1,20 @@
+package baseballgame;
+
+public class GameMessage {
+    public static String GAME_START = "숫자 야구 게임을 시작합니다.";
+
+    public static String INPUT_NUMBER = "숫자를 입력해 주세요 : ";
+    public static String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public static String GAME_EXIT = "게임 종료";
+
+    public static String NOT_ANSWER = "낫싱";
+
+    public static String THREE_STRIKE = "3스트라이크";
+
+    public static String STRIKE = "스트라이크";
+
+    public static String BALL = "볼";
+
+}
